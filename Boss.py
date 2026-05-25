239290e8e2a0aebbab5111dcdee51fa9de87b440
RichardoOsmond <richardoosmond805@gmail.com>
2026-05-25 17:36:36 +0800

    Update Boss.py

--- Boss.py
+++ Boss.py
@@ -5,5 +5,5 @@ The boss battle scene itself will take place in another python file.
 
 Upon completion of the boss battle, a message will be shown.
 A decision tree might be implemented (taught in data structures).
-(Good exercise before proceeding with the real assignment in C++.
+(Good exercise before proceeding with the real assignment in C++).
 """
