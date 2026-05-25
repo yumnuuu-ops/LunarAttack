bc6c7d67c2badcf967d22fc59a8bc57dc2ee3517
RichardoOsmond <richardoosmond805@gmail.com>
2026-05-25 17:30:08 +0800

    Update Boss.py

--- Boss.py
+++ Boss.py
@@ -3,7 +3,7 @@ This will be made as a class.
 It will contain all the effects of the move that will be used on the players.
 The boss battle scene itself will take place in another python file.
 
-Upon completion of the boss battle, Yumnu's face will be shown.
+Upon completion of the boss battle, a message will be shown.
 A decision tree might be implemented (taught in data structures).
 (Good exercise before proceeding with the real assignment in C++.
 """
