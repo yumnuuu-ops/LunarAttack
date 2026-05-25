ba0002f2b4a5199d775b3a8a1292927b3214a0b9
RichardoOsmond <richardoosmond805@gmail.com>
2026-05-25 17:29:37 +0800

    Create Boss.py

---
+++ Boss.py
@@ -0,0 +1,9 @@
+"""
+This will be made as a class.
+It will contain all the effects of the move that will be used on the players.
+The boss battle scene itself will take place in another python file.
+
+Upon completion of the boss battle, Yumnu's face will be shown.
+A decision tree might be implemented (taught in data structures).
+(Good exercise before proceeding with the real assignment in C++.
+"""
