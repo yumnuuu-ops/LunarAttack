7890c2777ecace0516aa0bcda657f59d02c0b996
Kenzie-ly <kenzienathanng@gmail.com>
2026-05-30 02:52:12 +0800

    add 2 special effects
    1. fading effect
    2. shaking effect

--- Player.py
+++ Player.py
@@ -16,6 +16,8 @@ class Player:
 
     def takeDamage(self, damage):
         self.hp -= damage
+        if hasattr(self, 'trigger_shake') and self.trigger_shake:
+            self.trigger_shake(10, 15)
 
     #def changeFireMode(self):
 
